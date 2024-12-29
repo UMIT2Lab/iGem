457f59853ece45d1038cba24cb97999a90eb7350
Akif Ahsen Ozer <42844668+ozaksen@users.noreply.github.com>
2024-12-28 23:15:03 -0500

    Adding card for devices 1/2

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,14 +1,7 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
 import Map from './components/Map/Map'
+
 function App() {
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
-
-  return (
-      <Map/>
-
-  )
+  return <Map />
 }
 
 export default App
-
